ce63d175105b3916d709a538d336ea7929e0cbac
Coco-Horatio <112703786+Coco-Horatio@users.noreply.github.com>
2022-12-13 06:26:34 -0500

    just updating my thing

--- Anotherappfromwork.js
+++ Anotherappfromwork.js
@@ -2,6 +2,12 @@ let PasswordDB = "juanitoalimaña123"
 let imput  = "Juanitoalimaña123"
 let result = PasswordDB == imput;
 
-if (result = true){
+if (result == true){
     console.log("tas claro");
+}
+if (result == false){
+    console.log("WRRRRONG!!!");
+}
+else {
+    console.log("incorrect password");
 }
