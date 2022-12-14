952694fe6065bc5b6b8c2511c86cdea86d4c2359
Coco-Horatio <112703786+Coco-Horatio@users.noreply.github.com>
2022-12-14 07:59:07 -0500

    here I was tying to create an app that shows the status of the player in relation to their score

---
+++ Punctuation.js
@@ -0,0 +1,24 @@
+{
+    let userName = Alvaro
+    let points = 153
+    let hoursPlayed = 42
+    let ProGramer = true
+}
+
+let points = 153
+
+if (points < 50) { 
+    console.log(String(userName)+" you just need some more practice");
+}
+
+else if (points < 25) {
+    console.log ("let train")
+}
+
+if (points < 100) and (points > 50) {
+    console.log(string(userName) + " your surely are improving");
+}
+
+else if { 
+
+}
