873693a00f6a2762cb4da39ff40067b566355e7a
Coco-Horatio <112703786+Coco-Horatio@users.noreply.github.com>
2022-12-16 06:19:12 -0500

    created a type card detect

---
+++ typeOfCard.js
@@ -0,0 +1,13 @@
+let typeofcard = "credit card";
+
+switch(typeofcard) {
+    case 'cebit card':
+         console.log('this is a Debit card')
+         break
+}
+switch (typeofcard){
+    case "credit card":
+         console.log("this is a credit card");
+         break
+}
+    
