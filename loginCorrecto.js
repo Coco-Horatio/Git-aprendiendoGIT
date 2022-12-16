da9e3ee99b155d30191b138a6565cc1cce31d907
Coco-Horatio <112703786+Coco-Horatio@users.noreply.github.com>
2022-12-16 05:30:52 -0500

    I created again the Login app

---
+++ loginCorrecto.js
@@ -0,0 +1,10 @@
+let Password = 'Arnobio740'
+let input = 'arnoboi740'
+
+let result = (imput == Password );
+
+if (result = true) {
+    console.log('Login correcto');
+} else {
+    console.log ('Login incorrecto');
+}
